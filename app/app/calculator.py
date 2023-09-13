2c8c7d8c7f0973308637c552f6222f957352b200
unknown <suman.s1701@gmail.com>
2023-09-13 14:52:53 +0530

    added custom command to wait for database to be raedy

--- app/app/calculator.py
+++ app/app/calculator.py
@@ -2,10 +2,12 @@
 Calculator functions
 """
 
-def add(x,y):
+
+def add(x, y):
     """Add x and y and return the result"""
     return x+y
 
-def substract(x,y):
+
+def substract(x, y):
     """Return the value y-x"""
     return y-x
--- app/app/tests.py
+++ app/app/tests.py
@@ -3,14 +3,14 @@
 from django.test import SimpleTestCase
 from app import calculator
 
+
 class TestCalculator(SimpleTestCase):
     """Test the calculator functions"""
-
     def test_add_numbers(self):
         """Test for add function in the calculator.py"""
-        result= calculator.add(15,61)
+        result = calculator.add(15, 61)
         self.assertEqual(result, 76)
 
     def test_sub_numbers(self):
-        result= calculator.substract(15,25)
+        result = calculator.substract(15, 25)
         self.assertEqual(result, 10)
